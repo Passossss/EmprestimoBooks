6f80be2db5591a17f28ea73bf0b6c284112d3178
Gustavo Passos <gusapas26@gmail.com>
2025-01-29 15:28:33 -0300

    update code

---
+++ SistemaLivros/Controllers/EmprestimoController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using SistemaLivros.Models;
+
+namespace SistemaLivros.Controllers
+{
+    public class EmprestimoControllers
+    {
+        public void EmprestarLivro(string recebedor, string fornecedor, string livroEmprestado)
+        {
+            EmprestimoModel emprestimo = new EmprestimoModel();
+            emprestimo.Recebedor = recebedor;
+            emprestimo.Fornecedor = fornecedor;
+            emprestimo.LivroEmprestado = livroEmprestado;
+            emprestimo.DataEmprestimo = DateTime.Now;
+        }
+    }
+    public class EmprestimoController : Controller
+    {
+        private readonly ILogger<EmprestimoController> _logger;
+
+        public EmprestimoController(ILogger<EmprestimoController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SistemaLivros/Models/EmprestimoModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+using System.Runtime.Intrinsics.X86;
+
+namespace SistemaLivros.Models
+{
+    public class EmprestimoModel
+    {
+        public int Id { get; set; }
+        public string Recebedor { get; set; }
+        public string Fornecedor { get; set; }
+        public string LivroEmprestado { get; set; }
+        public DateTime DataEmprestimo { get; set; } = DateTime.Now;
+
+    }
+}
