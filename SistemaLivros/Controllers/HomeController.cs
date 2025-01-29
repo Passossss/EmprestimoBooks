71627043ae988dce0aed016215ff917c67c5f9cc
Gustavo Passos <gusapas26@gmail.com>
2025-01-29 16:39:03 -0300

    add pages contato, teste, emprestimo

--- SistemaLivros/Controllers/HomeController.cs
+++ SistemaLivros/Controllers/HomeController.cs
@@ -22,6 +22,14 @@ namespace SistemaLivros.Controllers
         {
             return View();
         }
+        public IActionResult Teste()
+        {
+            return View();
+        }
+        public IActionResult Sobre()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
