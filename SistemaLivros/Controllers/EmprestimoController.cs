d77b7cae7cc1e363c8cff522dbdf4e13f582aa7c
Gustavo Passos <gusapas26@gmail.com>
2025-01-29 22:42:53 -0300

    model formularios criados + validacao campo

--- SistemaLivros/Controllers/EmprestimoController.cs
+++ SistemaLivros/Controllers/EmprestimoController.cs
@@ -1,28 +1,45 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using SistemaLivros.Models;
+using SistemaLivros.Data;
 
 namespace SistemaLivros.Controllers
 {
     public class EmprestimoController : Controller
     {
-        private readonly ILogger<EmprestimoController> _logger;
-
-        public EmprestimoController(ILogger<EmprestimoController> logger)
+        readonly private ApplicationDbContext _db;
+        public EmprestimoController(ApplicationDbContext db)
         {
-            _logger = logger;
+            _db = db;
         }
-
         public IActionResult Index()
+        {
+            IEnumerable<EmprestimoModel> emprestimos = _db.Emprestimos;
+            return View(emprestimos);
+        }
+        public IActionResult Cadastrar()
         {
             return View();
         }
 
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        [HttpPost]
+        public IActionResult Cadastrar(EmprestimoModel emprestimos)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            if(ModelState.IsValid)
+            {
+                _db.Emprestimos.Add(emprestimos);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
         }
+
+
+        //private readonly ILogger<EmprestimoController> _logger;
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
     }
 }
